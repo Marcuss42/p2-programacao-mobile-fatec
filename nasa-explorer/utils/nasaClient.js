f6ecf25218ece51bac1b7d162f6a219ea952a5b2
Marcus Paulo <mpfrancisco2003@gmail.com>
2025-11-28 06:33:16 -0300

    finish: Finalizado

--- nasa-explorer/utils/nasaClient.js
+++ nasa-explorer/utils/nasaClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export default axios.create({
   baseURL: 'http://localhost:3000/',
-  baseURL: 'http://192.168.1.106:3000'
+  // baseURL: 'http://192.168.1.106:3000' Coloquei isto para testar no celular, pois localhost não funciona
 })
